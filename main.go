c7b9e8fee3e959e478ec3755551a3b4f3efb5ed9
vincentysc <vincent.ys.chan@crypto.com>
2023-12-05 16:32:56 +0800

    print evm address

--- main.go
+++ main.go
@@ -9,18 +9,20 @@ import (
 
 func main() {
 	config := sdk.GetConfig()
-	config.SetBech32PrefixForAccount("cro", "cropub")
-	config.SetBech32PrefixForValidator("crocncl", "crocncl")
-	config.SetBech32PrefixForConsensusNode("crocnclcons", "crocnclconspub")
+	config.SetBech32PrefixForAccount("canto", "cantopub")
+	// config.SetBech32PrefixForAccount("cro", "cropub")
+	// config.SetBech32PrefixForValidator("crocncl", "crocncl")
+	// config.SetBech32PrefixForConsensusNode("crocnclcons", "crocnclconspub")
 	config.Seal()
 
-	// // Account address => Operator address
-	// accAddr, _ := sdk.AccAddressFromBech32("cro1amcxa06n0djycz0qd2cvvdyvd35uunnf6lya2x")
-	// valAddr, _ := sdk.ValAddressFromHex(hex.EncodeToString(accAddr.Bytes()))
-	// fmt.Println("Operator address:", valAddr.String())
+	// Account address => Operator address
+	accAddr, _ := sdk.AccAddressFromBech32("canto184h3psqp3ud6hp8n8z06sfm8khcv9k60m6kdv7")
+	valAddr, _ := sdk.ValAddressFromHex(hex.EncodeToString(accAddr.Bytes()))
+	fmt.Println("Operator address:", valAddr.String())
+	fmt.Println("Evm address:", hex.EncodeToString(accAddr.Bytes()))
 
-	// Operator address => account address
-	valAddr, _ := sdk.ValAddressFromBech32("crocncl1amcxa06n0djycz0qd2cvvdyvd35uunnfej85g6")
-	accAddr, _ := sdk.AccAddressFromHexUnsafe(hex.EncodeToString(valAddr.Bytes()))
-	fmt.Println("Account address:", accAddr.String())
+	// // Operator address => account address
+	// valAddr, _ := sdk.ValAddressFromBech32("crocncl1amcxa06n0djycz0qd2cvvdyvd35uunnfej85g6")
+	// accAddr, _ := sdk.AccAddressFromHexUnsafe(hex.EncodeToString(valAddr.Bytes()))
+	// fmt.Println("Account address:", accAddr.String())
 }
